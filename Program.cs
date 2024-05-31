626718cf6dadd35440e32fae4802513d2403df09
Dharani Karthick <dharanik305@gmail.com>
2024-05-31 14:59:03 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using ChatApp;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    //endpoints.MapControllers();
+    endpoints.MapHub<ChatHub>("/chatsocket");     // path will look like this https://localhost:44379/chatsocket 
+});
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+
+app.Run();
---
+++ wwwroot/js/chat.js
@@ -0,0 +1,35 @@
+﻿"use strict";
+
+//<script src="https://cdnjs.cloudflare.com/ajax/libs/microsoft-signalr/5.0.0/signalr.min.js"></script>
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatsocket").build();
+
+connection.on("ReceiveMessage", function (user, message) {
+    var encodedUser = $("<div />").text(user).html();
+    var encodedMessage = $("<div />").text(message).html();
+    var li = document.createElement('li');
+    var currentUser = $("#username").val();
+    // Add the username and message content to the list item
+    li.innerHTML = `<span class="username">` + encodedUser + `</span><br/>`
+                   +`<span class="message">` + encodedMessage + `</span>`;
+
+    // Check if the message is from the current user
+    if (encodedUser == currentUser) {
+        // Add the "sent" class to align the message to the right
+        li.classList.add('sent');
+    }
+    $("#messages").append(li);
+});
+
+connection.start().catch(function (err) {
+    console.error(err.toString());
+});
+
+$("#sendButton").click(function () {
+    var user = $("#username").val();
+    var message = $("#messageInput").val();
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        console.error(err.toString());
+    });
+    $("#messageInput").val("");
+});
